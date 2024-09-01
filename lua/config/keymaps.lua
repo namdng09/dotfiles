c0daddc2ecbddfb1f89897a3447e20b36204b102
namdng09 <namduong090104@gmail.com>
2024-09-01 11:52:01 +0700

    feat: new keymaps for convenient coding

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,28 +3,46 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Remove search highlights after searching
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Remove search highlights" })
+local keymap = vim.keymap -- for conciseness
 
--- Exit Vim's terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+---------------------
+-- General Keymaps -------------------
 
--- OPTIONAL: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- use jj to exit insert mode
+keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jk" })
 
--- Better window navigation
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+keymap.set("i", "kk", "<ESC>:w<CR>", { desc = "Exit insert mode and save with kk" })
 
--- Easily split windows
-vim.keymap.set("n", "<leader>wv", ":vsplit<cr>", { desc = "[W]indow Split [V]ertical" })
-vim.keymap.set("n", "<leader>wh", ":split<cr>", { desc = "[W]indow Split [H]orizontal" })
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
--- Stay in indent mode
-vim.keymap.set("v", "<", "<gv", { desc = "Indent left in visual mode" })
-vim.keymap.set("v", ">", ">gv", { desc = "Indent right in visual mode" })
+-- delete single character without copying into register
+-- keymap.set("n", "x", '"_x')
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move forward 1/2 a screen and in the middle" })
+keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move back 1/2 a screen and in the middle" })
+keymap.set("n", "n", "nzzzv", { desc = "jump to the next instance of a word and in the middle" })
+keymap.set("n", "N", "Nzzzv", { desc = "jump to the previous instance of a word and in the middle" })
+keymap.set("n", "*", "*zzzv", { desc = "jump to the next instance of a word and in the middle" })
+keymap.set("n", "#", "#zzzv", { desc = "jump to the previous instance of a word and in the middle" })
+
+keymap.set("n", "<leader>o", "o<Esc>", { desc = "Create a new line below and still in normal mode" })
+keymap.set("n", "<leader>O", "O<Esc>", { desc = "Create a new line above and still in normal mode" })
+
+keymap.set("n", "<leader>rn", ":IncRename ")
