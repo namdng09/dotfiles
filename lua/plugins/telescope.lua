0a02705fad9885070d1fc1dc02da4c3a871637b1
namdng09 <namduong090104@gmail.com>
2024-09-01 16:30:26 +0700

    fix: change the dependencies and set key map for using telescope.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,59 +1,38 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim',
-        -- pull a specific version of the plugin
-        tag = '0.1.6',
-        dependencies = {
-            -- general purpose plugin used to build user interfaces in neovim plugins
-            'nvim-lua/plenary.nvim'
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+    "folke/todo-comments.nvim",
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+
+    telescope.setup({
+      defaults = {
+        path_display = { "smart" },
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+            ["<C-j>"] = actions.move_selection_next, -- move to next result
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+          },
         },
-        config = function()
-            -- get access to telescopes built in functions
-            local builtin = require('telescope.builtin')
+      },
+    })
 
-            -- set a vim motion to <Space> + f + f to search for files by their names
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "[F]ind [F]iles"})
-            -- set a vim motion to <Space> + f + g to search for files based on the text inside of them
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "[F]ind by [G]rep"})
-            -- set a vim motion to <Space> + f + d to search for Code Diagnostics in the current project
-            vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
-            -- set a vim motion to <Space> + f + r to resume the previous search
-            vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = '[F]inder [R]esume' })
-            -- set a vim motion to <Space> + f + . to search for Recent Files
-            vim.keymap.set('n', '<leader>f.', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
-            -- set a vim motion to <Space> + f + b to search Open Buffers
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind Existing [B]uffers' })
-        end
-    },
-    {
-        'nvim-telescope/telescope-ui-select.nvim',
-        config = function()
-            -- get access to telescopes navigation functions
-            local actions = require("telescope.actions")
+    telescope.load_extension("fzf")
 
-            require("telescope").setup({
-                -- use ui-select dropdown as our ui
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown {}
-                    }
-                },
-                -- set keymappings to navigate through items in the telescope io
-                mappings = {
-                    i = {
-                         -- use <cltr> + n to go to the next option
-                        ["<C-n>"] = actions.cycle_history_next,
-                        -- use <cltr> + p to go to the previous option
-                        ["<C-p>"] = actions.cycle_history_prev,
-                        -- use <cltr> + j to go to the next preview
-                        ["<C-j>"] = actions.move_selection_next,
-                        -- use <cltr> + k to go to the previous preview
-                        ["<C-k>"] = actions.move_selection_previous,
-                    }
-                },
-                -- load the ui-select extension
-                require("telescope").load_extension("ui-select")
-            })
-        end
-    }
+    -- set keymaps
+    local keymap = vim.keymap -- for conciseness
+
+    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+  end,
 }
