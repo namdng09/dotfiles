22a9d7cc910e7b70fd5df5a291bfb4b65d8d5c2c
namdng09 <namduong090104@gmail.com>
2024-09-01 11:54:52 +0700

    fix: it can autopairs for java file

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,25 +1,30 @@
 return {
-    "windwp/nvim-autopairs",
-    event = { "InsertEnter" },
-    dependencies = {
-        "hrsh7th/nvim-cmp",
-    },
-    config = function()
-        -- Call the autopairs setup function to configure how we want autopairs to work
-        require'nvim-autopairs'.setup({
-            check_ts = true,
-            ts_config = {
-                lua = { "string" },
-                javascript = { "template_string" },
-                java = false,
-            }
-        })
+  "windwp/nvim-autopairs",
+  event = { "InsertEnter" },
+  dependencies = {
+    "hrsh7th/nvim-cmp",
+  },
+  config = function()
+    -- import nvim-autopairs
+    local autopairs = require("nvim-autopairs")
 
-        -- Get access to auto pairs completion and cmp plugins
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-        local cmp = require("cmp")
+    -- configure autopairs
+    autopairs.setup({
+      check_ts = true, -- enable treesitter
+      ts_config = {
+        lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+        javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
+        java = false, -- don't check treesitter on java
+      },
+    })
 
-        -- Whenever we accept a choice from an autocompletion, make sure that any pairs are automatically closed
-        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-    end
+    -- import nvim-autopairs completion functionality
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+    -- import nvim-cmp plugin (completions plugin)
+    local cmp = require("cmp")
+
+    -- make autopairs and completion work together
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  end,
 }
