cc514d1f1d4f903985dd1f057dead1a3cc5c543b
namdng09 <namduong090104@gmail.com>
2025-03-03 13:27:55 +0700

    fix: remove tsserver by ts_ls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,79 +1,106 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            -- setup mason with default properties
-            require("mason").setup()
-        end
-    },
-    -- mason lsp config utilizes mason to automatically ensure lsp servers you want installed are installed
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            -- ensure that we have lua language server, typescript launguage server, java language server, and java test language server are installed
-            require("mason-lspconfig").setup({
-                ensure_installed = { "eslint", "ts_ls", "lua_ls", "jdtls" },
-            })
-        end
-    },
-    -- mason nvim dap utilizes mason to automatically ensure debug adapters you want installed are installed, mason-lspconfig will not automatically install debug adapters for us
-    {
-        "jay-babu/mason-nvim-dap.nvim",
-        config = function()
-            -- ensure the java debug adapter is installed
-            require("mason-nvim-dap").setup({
-                ensure_installed = { "java-debug-adapter", "java-test" }
-            })
-        end
-    },
-    -- utility plugin for configuring the java language server for us
-    {
-        "mfussenegger/nvim-jdtls",
-        dependencies = {
-            "mfussenegger/nvim-dap",
-        }
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            -- get access to the lspconfig plugins functions
-            local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			-- setup mason with default properties
+			require("mason").setup()
+		end,
+	},
+	-- mason lsp config utilizes mason to automatically ensure lsp servers you want installed are installed
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			-- ensure that we have lua language server, typescript launguage server, java language server, and java test language server are installed
+			require("mason-lspconfig").setup({
+				ensure_installed = { "eslint", "ts_ls", "lua_ls", "jdtls" },
+			})
+		end,
+	},
+	-- mason nvim dap utilizes mason to automatically ensure debug adapters you want installed are installed, mason-lspconfig will not automatically install debug adapters for us
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		config = function()
+			-- ensure the java debug adapter is installed
+			require("mason-nvim-dap").setup({
+				ensure_installed = { "java-debug-adapter", "java-test" },
+			})
+		end,
+	},
+	-- utility plugin for configuring the java language server for us
+	{
+		"mfussenegger/nvim-jdtls",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+		},
+	},
 
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			-- get access to the lspconfig plugins functions
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            -- setup the lua language server
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities,
-            })
+			-- setup the lua language server
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
 
-            -- setup the tsserver language server (TypeScript/JavaScript)
-            lspconfig.ts_ls.setup({
-                on_attach = function(client, bufnr)
-                    -- Customize keymaps or disable formatting if needed
-                    client.resolved_capabilities.document_formatting = false
-                end,
-                capabilities = capabilities,
-                init_options = {
-                    preferences = {
-                        disableSuggestions = true, -- Example preference
-                    },
-                },
-            })
+			-- setup the TypeScript/JavaScript server
+			lspconfig.ts_ls.setup({
+				on_attach = function(client, bufnr)
+					-- Update the new field for disabling formatting
+					client.server_capabilities.documentFormattingProvider = false
+				end,
+				capabilities = capabilities,
+				init_options = {
+					preferences = {
+						disableSuggestions = true,
+					},
+				},
+			})
 
-            -- Set vim motion for <Space> + c + h to show code documentation about the code the cursor is currently over if available
-            vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
-            -- Set vim motion for <Space> + c + d to go where the code/variable under the cursor was defined
-            vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "[C]ode Goto [D]efinition" })
-            -- Set vim motion for <Space> + c + a for display code action suggestions for code diagnostics in both normal and visual mode
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
-            -- Set vim motion for <Space> + c + r to display references to the code under the cursor
-            vim.keymap.set("n", "<leader>cr", require("telescope.builtin").lsp_references, { desc = "[C]ode Goto [R]eferences" })
-            -- Set vim motion for <Space> + c + i to display implementations to the code under the cursor
-            vim.keymap.set("n", "<leader>ci", require("telescope.builtin").lsp_implementations, { desc = "[C]ode Goto [I]mplementations" })
-            -- Set a vim motion for <Space> + c + <Shift>R to smartly rename the code under the cursor
-            vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
-            -- Set a vim motion for <Space> + c + <Shift>D to go to where the code/object was declared in the project (class file)
-            vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
-        end
-    },
+			-- setup Java LSP (jdtls)
+			lspconfig.jdtls.setup({
+				capabilities = capabilities,
+				cmd = { "jdtls" }, -- Ensure jdtls is installed
+				root_dir = lspconfig.util.root_pattern("pom.xml", "gradle.build", ".git"),
+				settings = {
+					java = {
+						signatureHelp = { enabled = true },
+						contentProvider = { preferred = "fernflower" },
+					},
+				},
+			})
+
+			-- Automatically start jdtls in Java files
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "java",
+				callback = function()
+					vim.cmd("LspStart jdtls")
+				end,
+			})
+
+			-- Key mappings
+			local opts = { noremap = true, silent = true, desc = "[C]ode" }
+
+			vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
+			vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "[C]ode Goto [D]efinition" })
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
+			vim.keymap.set(
+				"n",
+				"<leader>cr",
+				require("telescope.builtin").lsp_references,
+				{ desc = "[C]ode Goto [R]eferences" }
+			)
+			vim.keymap.set(
+				"n",
+				"<leader>ci",
+				require("telescope.builtin").lsp_implementations,
+				{ desc = "[C]ode Goto [I]mplementations" }
+			)
+			vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
+			vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
+		end,
+	},
 }
