88be46fd3b0de8bf66b1e99c767b65b1c8969e35
namdng09 <namduong090104@gmail.com>
2025-03-19 13:27:02 +0700

    feat: new plugin for jsx auto tags

---
+++ lua/plugins/jsx-element.lua
@@ -0,0 +1,9 @@
+return {
+  'mawkler/jsx-element.nvim',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
+  ft = { 'typescriptreact', 'javascriptreact', 'javascript' },
+  opts = {},
+}
