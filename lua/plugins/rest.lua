05517b381b3acafb3f60cf89f287040eba940041
namdng09 <namduong090104@gmail.com>
2025-04-05 10:26:22 +0700

    fix: change format json from gq to jq

--- lua/plugins/rest.lua
+++ lua/plugins/rest.lua
@@ -7,6 +7,69 @@ return {
       table.insert(opts.ensure_installed, "http")
     end,
   },
+  config = function()
+    local rest_nvim = require("rest-nvim")
+
+    rest_nvim.setup({
+      response = {
+        hooks = {
+          decode_url = true,
+          format = true, -- dùng gq, sẽ hoạt động nếu formatprg thiết lập đúng
+        },
+      },
+      clients = {
+        curl = {
+          statistics = {
+            { id = "time_total",    winbar = "take", title = "Time taken" },
+            { id = "size_download", winbar = "size", title = "Download size" },
+          },
+          opts = {
+            set_compressed = false,
+            certificates = {},
+          },
+        },
+      },
+      cookies = {
+        enable = true,
+        path = vim.fs.joinpath(vim.fn.stdpath("data"), "rest-nvim.cookies"),
+      },
+      env = {
+        enable = true,
+        pattern = ".*%.env.*",
+        find = function()
+          local config = require("rest-nvim.config")
+          return vim.fs.find(function(name, _)
+            return name:match(config.env.pattern)
+          end, {
+            path = vim.fn.getcwd(),
+            type = "file",
+            limit = math.huge,
+          })
+        end,
+      },
+      ui = {
+        winbar = true,
+        keybinds = {
+          prev = "H",
+          next = "L",
+        },
+      },
+      highlight = {
+        enable = true,
+        timeout = 750,
+      },
+      _log_level = vim.log.levels.WARN,
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "json",
+      callback = function()
+        vim.bo.formatexpr = ""
+        vim.bo.formatprg = "jq"
+      end,
+    })
+  end,
+
   keys = {
     { "<leader>rr", "<cmd>Rest run<cr>",        desc = "Rest: Run request" },
     { "<leader>rl", "<cmd>Rest last<cr>",       desc = "Rest: Run last request" },
